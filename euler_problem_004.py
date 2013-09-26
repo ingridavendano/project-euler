e7c062f0f79b9f5c953815578621313511e6c330
Ingrid Avendaño <ingridavendano@gmail.com>
2013-09-25 19:41:39 -0700

    Solved problem 4.
    Answer is 993 x 913 = 906609.

--- euler_problem_004.py
+++ euler_problem_004.py
@@ -4,7 +4,49 @@
 # Project Euler - Problem 4:
 #
 # A palindromic number reads the same both ways. The largest palindrome made from 
-# the product of two 2-digit numbers is 9009 = 91 × 99.
+# the product of two 2-digit numbers is 9009 = 91 x 99.
 
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
+def isPalindrone(x):
+	x = str(x)
+	length = len(x)
+	result = True
+
+	# checks digits from the beginning against those at the end to see if they 
+	# match each other
+	for i in range(length/2):
+		if x[i] != x[length - 1 - i]:
+			result = False
+			break
+
+	return result
+
+# start with the highest three digit numbers
+a = 999
+b = 999
+product = 0
+highest_palindrone_product = 0
+
+num1 = a
+num2 = b
+
+count = 0
+
+# iterate through 999-0
+for i in range(a,99,-1): 
+
+	# iterate through 999-0
+	for j in range(b,99,-1):
+		product = i*j
+		count += 1
+
+		# checks if product is the highest valued palidrone
+		if isPalindrone(product):
+			if product > highest_palindrone_product:
+				highest_palindrone_product = product
+				num1 = i
+				num2 = j
+
+print "Solved! %d x %d = %d" % (num1,num2,highest_palindrone_product)
+print "It took %d times" % count
