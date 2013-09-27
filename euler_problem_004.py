39257300d996d197b0f74430ab49f6fcfca8fa61
Ingrid Avendaño <ingridavendano@gmail.com>
2013-09-27 01:46:38 -0700

    Largest palindrome product
    906609

--- euler_problem_004.py
+++ euler_problem_004.py
@@ -1,12 +1,13 @@
 # Created by Ingrid Avendano on 9/24/13. 
 #
 #
-# Project Euler - Problem 4:
+# Project Euler - Problem 4: Largest palindrome product
 #
 # A palindromic number reads the same both ways. The largest palindrome made 
 # from the product of two 2-digit numbers is 9009 = 91 x 99.
 #
 # Find the largest palindrome made from the product of two 3-digit numbers.
+
 
 def is_a_palindrone(num):
 	num = str(num)
