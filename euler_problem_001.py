80e03de5d3bf7a630a38b451ad7bbb7eeb210b22
Ingrid Avendaño <ingridavendano@gmail.com>
2013-09-27 00:34:09 -0700

    Solved problem 1.
    233168

--- euler_problem_001.py
+++ euler_problem_001.py
@@ -8,10 +8,13 @@
 #
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-total = 0
+def main(): 
+	total = 0
 
-for i in range(1000):
-	if i%3==0 or i%5==0:
-		total += i
+	for i in range(1000):
+		if i%3==0 or i%5==0:
+			total += i
 
-print total
+	print "Solution to problem 1:", total
+
+main()
