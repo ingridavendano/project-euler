1eece038a55d9fea9b0cf0789a4982af0c13d8a4
Ingrid Avendaño <ingridavendano@gmail.com>
2013-09-27 01:28:52 -0700

    Solved
    906609

--- euler_problem_004.py
+++ euler_problem_004.py
@@ -3,47 +3,46 @@
 #
 # Project Euler - Problem 4:
 #
-# A palindromic number reads the same both ways. The largest palindrome made from 
-# the product of two 2-digit numbers is 9009 = 91 x 99.
+# A palindromic number reads the same both ways. The largest palindrome made 
+# from the product of two 2-digit numbers is 9009 = 91 x 99.
 #
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-def is_palindrone(x):
-	x = str(x)
-	length = len(x)
+def is_a_palindrone(num):
+	num = str(num)
+	length_of_num = len(num)
 	result = True
 
 	# checks digits from the beginning against those at the end to see if they 
 	# match each other
-	for i in range(length/2):
-		if x[i] != x[length - 1 - i]:
+	for i in range(length_of_num/2):
+
+		if num[i] != num[-1-i]:
 			result = False
 			break
 
 	return result
 
-# start with the highest three digit numbers
-product = 0
-highest_palindrone_product = 0
 
-num1 = 0
-num2 = 0
-count = 0
+def main():
+	# start with the highest three digit numbers
+	highest_palindrone_product = 0
+	product = 0
 
-# iterate through 999 to 0
-for i in range(999,99,-1): 
+	# iterate through 999 to 0
+	for i in range(999,99,-1): 
 
-	# iterate through whatever i is to 0
-	for j in range(i,99,-1):
-		product = i*j
-		count += 1
+		# iterate through whatever i is to 0
+		for j in range(i,99,-1):
+			product = i*j
+			# count += 1
 
-		# checks if product is the highest valued palidrone
-		if is_palindrone(product):
-			if product > highest_palindrone_product:
-				highest_palindrone_product = product
-				num1 = i
-				num2 = j
+			# checks if product is the highest valued palidrone
+			if is_a_palindrone(product):
+				if product > highest_palindrone_product:
+					highest_palindrone_product = product
 
-print "Solved! %d x %d = %d" % (num1,num2,highest_palindrone_product)
-print "It took %d times" % count
+	print "Solution to problem 4:", highest_palindrone_product
+	
+
+main()
