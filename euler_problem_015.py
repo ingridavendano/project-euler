1e2b305b747c75e2f77b707b8af8f53b0174603e
Ingrid Avendaño <ingridavendano@gmail.com>
2013-09-29 03:44:48 -0700

    Starting problem 15.

---
+++ euler_problem_015.py
@@ -0,0 +1,20 @@
+# Created by Ingrid Avendano on 9/29/13. 
+#
+#
+# Project Euler - Problem 15: 
+#
+# Starting in the top left corner of a 2×2 grid, and only being able 
+# to move to the right and down, there are exactly 6 routes to the bottom
+# right corner.
+#
+# *** Image of grid at: http://projecteuler.net/problem=15
+#
+# How many such routes are there through a 20×20 grid?
+
+
+def main():
+
+	print "Solution to problem 14:"
+
+
+main()
