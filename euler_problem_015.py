ed268ed0ff34894ab8d0f57c5512a6d6ad970037
Ingrid Avendaño <ingridavendano@gmail.com>
2013-09-29 04:08:48 -0700

    ***** IN PROGRESS *****

--- euler_problem_015.py
+++ euler_problem_015.py
@@ -1,20 +1,20 @@
 # Created by Ingrid Avendano on 9/29/13. 
 #
 #
-# Project Euler - Problem 15: 
+# Project Euler - Problem 15: Lattice paths
 #
-# Starting in the top left corner of a 2×2 grid, and only being able 
+# Starting in the top left corner of a 2x2 grid, and only being able 
 # to move to the right and down, there are exactly 6 routes to the bottom
 # right corner.
 #
 # *** Image of grid at: http://projecteuler.net/problem=15
 #
-# How many such routes are there through a 20×20 grid?
+# How many such routes are there through a 20x20 grid?
 
 
 def main():
 
-	print "Solution to problem 14:"
 
+	print "Solution to problem 15:"
 
 main()
