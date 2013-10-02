5519e718d2e7890a29538abb0f8c2a2ea080aac5
Ingrid Avendaño <ingridavendano@gmail.com>
2013-10-02 02:23:30 -0700

    Revert "Revert "Starting problem 19.""
    This reverts commit 479bec7cf9e28c728a5d196e23f67676332d43c4.

---
+++ euler_problem_019.py
@@ -0,0 +1,49 @@
+# Created by Ingrid Avendano on 9/29/13. 
+#
+#
+# Project Euler - Problem 19: Counting Sundays
+#
+# You are given the following information, but you may prefer to do some
+# research for yourself.
+#
+# 1 Jan 1900 was a Monday.
+# Thirty days has September,
+# April, June and November.
+# All the rest have thirty-one,
+# Saving February alone,
+# Which has twenty-eight, rain or shine.
+# And on leap years, twenty-nine.
+# A leap year occurs on any year evenly divisible by 4, but not on a 
+# century unless it is divisible by 400.
+# How many Sundays fell on the first of the month during the twentieth 
+# century (1 Jan 1901 to 31 Dec 2000)?
+
+
+def days_in_month(month, year):
+	# checks if month is September,April, June and November.
+	if month in [9,4,6,11]:
+		return 30
+	# check January, March, May, July, August, October, December
+	elif month in [1,3,5,7,8,10,12]:
+		return 31
+	# check for February 
+	elif month is 2:
+		if year%4 == 0:
+			return 29
+		else:
+			return 28
+	return 0
+
+
+
+
+def main():
+
+	years = 99
+	starting_date = 1
+	ending_date = 31
+
+	# print "Solution to problem 19:"
+
+
+main()
