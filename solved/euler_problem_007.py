e123c7eadb218a79a386887598025db519cb491e
Ingrid Avendaño <ingridavendano@gmail.com>
2013-10-03 00:44:04 -0700

    10001st prime
    104743

---
+++ solved/euler_problem_007.py
@@ -0,0 +1,53 @@
+# Created by Ingrid Avendano on 9/26/13. 
+#
+#
+# Project Euler - Problem 7: 10001st prime
+#
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can
+# see that the 6th prime is 13.
+#
+# What is the 10 001st prime number?
+
+
+# checks for highest prime factor
+def is_prime_factor(num):
+	number_is_prime = True
+	i = 2
+
+	# only increment through numbers that are less or equal to 
+	# half of the original number 
+	while i*i <= num:
+		if num%i == 0:
+			number_is_prime = False
+			break
+		i += 1
+
+	return number_is_prime
+
+
+# finds the nth prime number
+def nth_prime(num):
+	prime_at_nth = 0
+	number_of_primes = 0
+	i = 2
+	
+	while number_of_primes < num:
+
+		# only increments the number_of_primes if it is a 
+		# prime number 
+		if is_prime_factor(i):
+			number_of_primes += 1
+
+		if num==number_of_primes:
+			prime_at_nth = i 
+
+		i += 1
+
+	return prime_at_nth
+
+
+def main():
+	solution = nth_prime(10001)
+	print "Solution to problem 7:", solution
+
+main()
