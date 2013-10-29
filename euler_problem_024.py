3fad731db5dd1a10cf7f96154ef0705de2a79fcc
Ingrid Avendaño <ingridavendano@gmail.com>
2013-10-28 18:52:34 -0700

    Lexicographic permutations

--- euler_problem_024.py
+++ euler_problem_024.py
@@ -1,4 +1,4 @@
-# Created by Ingrid Avendano on 10/2/13. 
+ # Created by Ingrid Avendano on 10/2/13. 
 #
 #
 # Project Euler - Problem 24: Lexicographic permutations
@@ -15,8 +15,50 @@
 # 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
 
+def lexicographic(list_of_digits, variations, depth):
+
+	if depth == len(list_of_digits):
+		return variations
+
+	depth += 1
+
+	numbers = list_of_digits
+	
+	for i in range(len(variations)):
+		for digit in numbers:
+			if not digit in variations[i]:
+				potential_variation = variations[i] + digit
+				if not potential_variation in variations:
+					variations[i] = potential_variation
+					break
+
+	# print "depth:", depth
+	# for variation in variations:
+	# 	print variation
+
+	return lexicographic(list_of_digits, variations, depth)
+
+
 def main():
-	pass
+	size = 3
+	digits = []
+
+	for i in range(size):
+		digits.append(str(i))
+
+	variations = []
+	for digit in range(size):
+		for variety in range(size-1):
+			variations.append(str(digit))
+
+
+	print digits
+	list_of_variations = lexicographic(digits, variations, 1)
+
+	for number in list_of_variations:
+		print number
+
 	# print "Solution to problem 24:"
+
 
 main()
