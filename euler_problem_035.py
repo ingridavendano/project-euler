75e377ad3e4ffb044740f4e830fa81ae1d17de8d
Ingrid Avendaño <ingridavendano@gmail.com>
2013-10-31 22:03:23 -0700

    Circular primes

---
+++ euler_problem_035.py
@@ -0,0 +1,20 @@
+# Created by Ingrid Avendano on 10/31/13. 
+#
+#
+# Project Euler - Problem 35: Circular primes
+#
+# The number, 197, is called a circular prime because all rotations 
+# of the digits: 197, 971, and 719, are themselves prime.
+#
+# There are thirteen such primes below 100: 2, 3, 5, 7, 11, 13, 17, 
+# 31, 37, 71, 73, 79, and 97.
+#
+# How many circular primes are there below one million?
+
+
+def main():
+
+	# print "Solution to problem 35:", solution
+	
+
+main()
