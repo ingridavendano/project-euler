1548c2f80ef0963d4a9de982aa69af4d8eba2c45
Ingrid Avendaño <ingridavendano@gmail.com>
2014-02-08 22:11:53 -0800

    Prime permutations

---
+++ euler_problem_049.py
@@ -0,0 +1,22 @@
+# Created by Ingrid Avendano on 2/8/14. 
+#
+#
+# Project Euler - Problem 49: Prime permutations
+#
+# The arithmetic sequence, 1487, 4817, 8147, in which each of the terms 
+# increases by 3330, is unusual in two ways: (i) each of the three terms are 
+# prime, and, (ii) each of the 4-digit numbers are permutations of one another.
+#
+# There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes,
+# exhibiting this property, but there is one other 4-digit increasing sequence.
+#
+# What 12-digit number do you form by concatenating the three terms in this
+# sequence?
+
+
+def main():
+	pass
+	# print "Solution to problem 49:", solution
+	
+
+main()
