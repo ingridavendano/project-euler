716fa4a6e9b3ef4de87e0f589c23e19a8ffc9d15
Ingrid Avendaño <ingridavendano@gmail.com>
2014-02-10 22:44:31 -0800

    In progress.

--- euler_problem_024.py
+++ euler_problem_024.py
@@ -15,48 +15,66 @@
 # 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
 
 
-def lexicographic(list_of_digits, variations, depth):
+# def lexicographic(list_of_digits, variations, depth):
 
-	if depth == len(list_of_digits):
-		return variations
+# 	if depth == len(list_of_digits):
+# 		return variations
 
-	depth += 1
+# 	depth += 1
 
-	numbers = list_of_digits
+# 	numbers = list_of_digits
 	
-	for i in range(len(variations)):
-		for digit in numbers:
-			if not digit in variations[i]:
-				potential_variation = variations[i] + digit
-				if not potential_variation in variations:
-					variations[i] = potential_variation
-					break
+# 	for i in range(len(variations)):
+# 		for digit in numbers:
+# 			if not digit in variations[i]:
+# 				potential_variation = variations[i] + digit
+# 				if not potential_variation in variations:
+# 					variations[i] = potential_variation
+# 					break
 
-	# print "depth:", depth
-	# for variation in variations:
-	# 	print variation
+# 	# print "depth:", depth
+# 	# for variation in variations:
+# 	# 	print variation
 
-	return lexicographic(list_of_digits, variations, depth)
+# 	return lexicographic(list_of_digits, variations, depth)
 
 
 def main():
-	size = 3
-	digits = []
+	nums = [str(i) for i in range(3)]
+	shift = len(nums) - 2
+	current = ""
+	count = 0
 
-	for i in range(size):
-		digits.append(str(i))
-
-	variations = []
-	for digit in range(size):
-		for variety in range(size-1):
-			variations.append(str(digit))
+	print nums[:shift] + nums[shift+1:] + [nums[shift]]
+	while count < 10:
+		print "".join(nums)
+		if shift == len(nums) - 1:
+			shift = 0
 
 
-	print digits
-	list_of_variations = lexicographic(digits, variations, 1)
+		nums = nums[:shift] + nums[shift+1:] + [nums[shift]]
+		shift += 1
+		count += 1
 
-	for number in list_of_variations:
-		print number
+	# print "".join(nums)
+
+	# size = 3
+	# digits = []
+
+	# for i in range(size):
+	# 	digits.append(str(i))
+
+	# variations = []
+	# for digit in range(size):
+	# 	for variety in range(size-1):
+	# 		variations.append(str(digit))
+
+
+	# print digits
+	# list_of_variations = lexicographic(digits, variations, 1)
+
+	# for number in list_of_variations:
+	# 	print number
 
 	# print "Solution to problem 24:"
 
